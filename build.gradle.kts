8a8caf0debd92ec4c7a78f1c617c88bae581179d
JeongYunSung <123dbstjd@gmail.com>
2025-01-28 14:04:49 +0900

    chore: jackson 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,9 @@ repositories {
 
 dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-jackson:2.11.0")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.18.2")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.18.2")
     testImplementation(kotlin("test"))
 }
 
