c41da56fefd0a72e1d0c57fb0cd77ca7aee01324
JeongYunSung <123dbstjd@gmail.com>
2025-01-28 13:49:19 +0900

    chore: 레트로핏 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,14 +2,15 @@ plugins {
     kotlin("jvm") version "2.0.20"
 }
 
-group = "org.example"
-version = "1.0-SNAPSHOT"
+group = "com.yunseong"
+version = "0.1"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
     testImplementation(kotlin("test"))
 }
 
